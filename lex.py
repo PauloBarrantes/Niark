2d09b1ba69d22246e1f50865c95ca6bfd809b8fb
Paulo <paulobarrante@gmail.com>
2018-03-27 14:50:56 -0600

    Lex.py ya lee el archivo y lo imprime. Code.nia es un ejemplo de código en Niark (Fibonacci)

--- lex.py
+++ lex.py
@@ -5,4 +5,8 @@ Prerequisitos: Tener instalado python 2.6 mínimo
 
 Correr el archivo lex.py, colocar el nombre del archivo .nia que contiene el código fuente del lenguaje de programación Niark
 """
+name = raw_input("Escriba el nombre del archivo con el código fuente ")
+file = open(name, 'r')
+line = file.read()
 
+print(line)
