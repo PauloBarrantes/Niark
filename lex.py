6f3cfc43336ab200e8adbcd4678768e2ee8700ab
Paulo <paulobarrante@gmail.com>
2018-04-04 23:52:27 -0600

    Encabezado

--- lex.py
+++ lex.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 """
 Nombre del equipo: Byte_Me
-Prerequisitos: Tener instalado python 2.6 mínimo
+Prerequisitos: Tener instalado python 3.6 
 
 Correr el archivo lex.py, colocar el nombre del archivo .nia que contiene el código fuente del lenguaje de programación Niark
 """
