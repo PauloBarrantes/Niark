cdd3c461e2a9e4bf7ee81ac269cbbd093137a9c6
Paulo César Barrantes Aguilar <paulobarrante@gmail.com>
2018-04-16 21:00:08 -0600

    Cambios en unas cosas del parser. Python 3.6

--- parser.py
+++ parser.py
@@ -4,13 +4,12 @@ import ply.yacc as yacc
 from lex import tokens
 
 
-
-def p_expression_plus(p):
-    'expression : expression PLUS term'
+def p_expression_suma(p):
+    'expression : expression SUMA term'
     p[0] = p[1] + p[3]
 
-def p_expression_minus(p):
-    'expression : expression MINUS term'
+def p_expression_resta(p):
+    'expression : expression RESTA term'
     p[0] = p[1] - p[3]
 
 def p_expression_term(p):
@@ -22,7 +21,7 @@ def p_term_times(p):
     p[0] = p[1] * p[3]
 
 def p_term_div(p):
-    'term : term DIVIDE factor'
+    'term : term DIV factor'
     p[0] = p[1] / p[3]
 
 def p_term_factor(p):
@@ -46,7 +45,7 @@ parser = yacc.yacc()
 
 while True:
    try:
-       s = raw_input('calc > ')
+       s = input('calc > ')
    except EOFError:
        break
    if not s: continue
