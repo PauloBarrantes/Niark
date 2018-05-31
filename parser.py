7e03bf528c085e4adb20e7325f24b15595d3bbde
Bailin Huang <bailin.huang@ucr.ac.cr>
2018-05-31 10:49:21 -0600

    Parser changes

--- parser.py
+++ parser.py
@@ -12,23 +12,40 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 #GENERAL
+
 def p_Niark(p):
     '''
     Niark : definicion_funcion LLAVEIZQ NEWLINE instrucciones LLAVEDER Niark
-          | instrucciones Niark
-          | vacio
+    '''
+
+def p_Niark1(p):
+    '''
+    Niark : instrucciones Niark
+    '''
+
+def p_Niark2(p):
+    '''
+    Niark : vacio
     '''
 
 def p_CAMBIOS_DE_LINEA(p):
     '''
     cambios_linea : NEWLINE cambios_linea
-                  | vacio
+    '''
+
+def p_CAMBIOS_DE_LINEA1(p):
+    '''
+    cambios_linea : vacio
     '''
 
 def p_INDENTACION(p):
     '''
     indentacion : TABULACION indentacion
-                | vacio
+    '''
+
+def p_INDENTACION1(p):
+    '''
+    indentacion : vacio
     '''
 
 #FUNCION
@@ -38,46 +55,72 @@ def p_DEFINICION_FUNCION(p):
 def p_DOMINIO_FUNC(p):
     '''
     dominio : PRIVATE
-            | PUBLIC
-    '''
+            |  PUBLIC
+    ''' 
 
 def p_RETURN_FUNC(p):
     '''
     tipo_return : VOID
-                | FUNCTION
     '''
+    print(p[1])
+
+def p_RETURN_FUNC1(p):
+    '''
+    tipo_return : FUNCTION
+    '''
+    print(p[1])
 
 def p_PARAMETRO(p):
     '''
     parametro : vacio
-              | NOMBRE parametro_extra
+    '''
+
+def p_PARAMETRO1(p):
+    '''
+    parametro : NOMBRE parametro_extra
 
     '''
 
 def p_PARAMETRO_EXTRA(p):
     '''
     parametro_extra : COMA NOMBRE parametro_extra
-                    | vacio
+    '''
+
+def p_PARAMETRO_EXTRA1(p):
+    '''
+    parametro_extra : vacio
     '''
 
 #INSTRUCCIONES
 def p_LLAMADO_FUNCION(p):
     '''
     llamado_funcion : NOMBRE PARIZQ parametro_llamado PARDER
-                    | NOMBRE PARIZQ vacio PARDER
+    '''
+
+def p_LLAMADO_FUNCION01(p):
+    '''
+    llamado_funcion : NOMBRE PARIZQ vacio PARDER
     '''
 
 def p_PARAMETRO_LLAMADO(p):
     '''
     parametro_llamado : tipo_variable parametro_llamado_extra
-                      | op_aritmetica parametro_llamado_extra
+    '''
+
+def p_PARAMETRO_LLAMADO1(p):
+    '''
+    parametro_llamado : op_aritmetica parametro_llamado_extra
 
     '''
 
 def p_PARAMETRO_LLAMADO_EXTRA(p):
     '''
     parametro_llamado_extra : COMA parametro_llamado
-                            | vacio
+    '''
+
+def p_PARAMETRO_LLAMADO_EXTRA1(p):
+    '''
+    parametro_llamado_extra : vacio
     '''
 
 def p_INSTRUCCION(p):
