a28f595933615217df77bd427c606243e3759d89
Ducan-Ally <Fakebian98@gmail.com>
2018-06-04 00:14:04 -0600

    Ultima version, errores de infinita recursion

--- newParse.py
+++ newParse.py
@@ -1,4 +1,15 @@
 import ply.yacc as yacc
+from newLex import tokens
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 #All the ways the program can start
 
@@ -21,13 +32,14 @@ def p_methodDefinition1(p):
     'methodDefinition : domain methodType NAME LEFTPAR parameters RIGHTPAR LEFTKEY newlines instructions RIGHTKEY'
 
 def p_methodDefinition2(p):
-    'methodDefinition : domain methodType NAME LEFTPAR VOID RIGHTPAR LEFTKEY newlines instructions RIGHTKEY'
+    'methodDefinition : domain methodType NAME LEFTPAR RIGHTPAR LEFTKEY newlines instructions RIGHTKEY'
 
 	
 	
 #Instructions definition
 def p_instructions(p):
-    'instructions: instruction newlines instructions'
+    'instructions : instruction newlines instructions'
+    'instructions : instruction newlines'
 	
 	
 	
@@ -121,10 +133,10 @@ def p_print(p):
 	
 #Definition of the systemcall instruction
 def p_functioncall1(p):
-    'functionCall: NAME LEFTPAR variables RIGHTPAR'
+    'functionCall : NAME LEFTPAR variables RIGHTPAR'
 	
 def p_functioncall2(p):
-    'functionCall: NAME LEFTPAR VOID RIGHTPAR'
+    'functionCall : NAME LEFTPAR empty RIGHTPAR'
 	
 	
 	
@@ -142,18 +154,110 @@ def p_complex3(p):
 	
 #Definition of the if conditional
 def p_ifCondition1(p):
-    'ifCondition : IF LEFTPAR conditionals RIGHTPAR LEFTKEY ignore instructions ignore RIGHTKEY'
+    'ifCondition : IF LEFTPAR conditionals RIGHTPAR LEFTKEY ignore instructions RIGHTKEY'
 
 def p_ifCondition2(p):
-    'ifCondition : IF LEFTPAR conditionals RIGHTPAR LEFTKEY ignore instructions ignore RIGHTKEY ignore ELSE LEFTKEY ignore instructions ignore RIGHTKEY'
+    'ifCondition : IF LEFTPAR conditionals RIGHTPAR LEFTKEY ignore instructions RIGHTKEY ignore ELSE LEFTKEY ignore instructions RIGHTKEY'
 	
 def p_ifCondition3(p):
-    'ifCondition : IF LEFTPAR conditionals RIGHTPAR LEFTKEY ignore instructions ignore RIGHTKEY ignore ELSE ignore ifCondition'
+    'ifCondition : IF LEFTPAR conditionals RIGHTPAR LEFTKEY ignore instructions RIGHTKEY ignore ELSE ignore ifCondition'
+	
 	
 	
 #Definition of the for conditional
 def p_forCondition(p):
-    'forCondition : FOR LEFTPAR declaration SEMICOLON conditionals SEMICOLON incdec RIGHTPAR LEFTKEY ignore instructions ignore RIGHTKEY'	
+    'forCondition : FOR LEFTPAR declaration SEMICOLON conditionals SEMICOLON incdec RIGHTPAR LEFTKEY ignore instructions RIGHTKEY'
+
+	
+	
+#Definition of the while conditional
+def p_whileCondition(p):
+    'whileCondition : WHILE LEFTPAR conditionals RIGHTPAR LEFTKEY ignore instructions RIGHTKEY'
+	
+
+
+#Conditionals definition
+def p_conditionals1(p):
+    'conditionals : condition'
+	
+def p_conditionals2(p):
+    'conditionals : condition conditionalOp conditionals'
+	
+def p_conditionals3(p):
+    'conditionals : LEFTPAR condition RIGHTPAR conditionalOp conditionals'
+	
+def p_conditionals4(p):
+    'conditionals : LEFTPAR condition RIGHTPAR'
+	
+	
+	
+#Condition definition
+def p_condition1(p):
+    'condition : variable conditionOp variable'
+	
+def p_condition2(p):
+    'condition : variable conditionOp variableType'
+	
+def p_condition3(p):
+    'condition : variableType conditionOp variable'
+	
+def p_condition4(p):
+    'condition : variableType conditionOp variableType'
+	
+
+
+#Increase decrease definition
+def p_incdec1(p):
+    'incdec : preIncdec'
+   
+def p_incdec2(p):
+    'incdec : postIncdec'
+
+
+
+#Pre Increase decrease definition
+def p_preIncdec1(p):
+    'preIncdec : INCREASE variable'
+	
+def p_preIncdec2(p):
+    'preIncdec : DECREASE variable'
+
+
+
+#Post Increase decrease definition
+def p_postIncdec1(p):
+    'postIncdec : variable INCREASE'
+	
+def p_postIncdec2(p):
+    'postIncdec : variable DECREASE'
+	
+	
+	
+	
+#Conditionals operators definition
+def p_conditionalOp1(p):
+    'conditionalOp : AND'
+	
+def p_conditionalOp2(p):
+    'conditionalOp : OR'
+	
+	
+	
+#Condition operators definition
+def p_conditionOp1(p):
+    'conditionOp : EQUALS'
+	
+def p_conditionOp2(p):
+    'conditionOp : LESSER'
+	
+def p_conditionOp3(p):
+    'conditionOp : GREATER'
+	
+def p_conditionOp4(p):
+    'conditionOp : LESSEREQUAL'
+	
+def p_conditionOp5(p):
+    'conditionOp : GREATEREQUAL'
 	
 	
 	
@@ -220,12 +324,13 @@ def p_variableType1(p):
     'variableType : numberType'
 	
 def p_variableType2(p):
-    'variableType : strngType'
+    'variableType : stringType'
 	
 def p_variableType3(p):
     'variableType : booleanType'
 	
 
+	
 #Number type definition
 def p_numberType1(p):
     'numberType : INT'
@@ -268,16 +373,29 @@ def p_moreArithmetic2(p):
 def p_arithmeticOp1(p):
     'arithmeticOp : SUM'
 	
-def p_arithmeticOp1(p):
+def p_arithmeticOp2(p):
     'arithmeticOp : SUBSTRACTION'
 	
-def p_arithmeticOp1(p):
+def p_arithmeticOp3(p):
     'arithmeticOp : MULTIPLICATION'
 	
-def p_arithmeticOp1(p):
+def p_arithmeticOp4(p):
     'arithmeticOp : DIVISION'
 
 
+
+#Newlines definition
+def p_newlines(p):
+    'newlines : NEWLINE ignore'
+	
+def p_ignore1(p):
+    'ignore : NEWLINE ignore'
+	
+def p_ignore1(p):
+    'ignore : empty'
+
+def p_empty(p):
+    'empty : '
 
 # Build the parser
 parser = yacc.yacc()
