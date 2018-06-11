94a38d39778f08e43856feac321544574d702512
Paulo Barrantes <paulobarrantes98@gmail.com>
2018-06-10 22:34:39 -0600

    Solucionado
    Parcialmente

--- semantic.py
+++ semantic.py
@@ -35,22 +35,27 @@ class decArray(): #En el AST la declaracion de arreglo está como declaración d
 def semantic():
     parse()
     niark = getNiarkCode()
+    globalScope = [];
+    listOfLists.insert(0,globalScope)
 
     for statement in niark.statements:
         recursive(statement, listOfLists)
 
 
 def recursive(object, listaDeListas):
+
     if type(object) is Method:
         name = object.name
         functionDomain = object.functionDomain
         returnType = object.returnType
         parameter = object.parameter
         decMethod1 = decMethod(name, functionDomain, returnType, parameter)
-        listaDeListas.insert(0,decMethod1)
+        listaDeListas[0].insert(0,decMethod1)
         print (object.name)
+
         newList = listOfLists[:]  #[:] hace que la asignacion sea por copia
-        for instruction in object.instructions
+
+        for instruction in object.instructions:
             recursive(instruction, newList)
     else:
         if type(object) is VariableDeclaration:
@@ -62,17 +67,17 @@ def recursive(object, listaDeListas):
                 value = object.variable.value
                 type1 = type(value)
                 decVar1 = decVar(name, value, type1)
-                listaDeListas.insert(0,decVar1)
+                listaDeListas[0].insert(0,decVar1)
         else:
             if type(object) is VariableAssignation:
-                if lookup(object.name,listOfLists):
+                if lookup(object.name,listaDeListas):
                     print("Es una asignacion, vamos a asignar el valor\n")
                     #Deberiamos buscar la ubicación de la variable y verificar que el value del object coincida con el de la variable
                 else:
                     print("Variable ",object.name, " no ha sido declarada")
             else:
                 if type(object) is FunctionCall:
-                    if lookup(object.name,listOfLists):
+                    if lookup(object.name,listaDeListas):
                         print("Es un llamado a funcion")
                         # Hay que buscar la ubicación del método y verificar que los parametros coincidan con los declarados
                     else:
@@ -80,24 +85,25 @@ def recursive(object, listaDeListas):
                 else:
                     if type(object) is If:
                         newList = listOfLists[:] #Asignamos copia de la tabla de simbolos
-                        for instruction in object.instructions
+                        for instruction in object.instructions:
                             recursive(instruction, newList)# Recursively check internal instructions
                     else:
                         if type(object) is IfAndElse:
                             newList = listOfLists[:] #Asignamos copia de la tabla de simbolos
-                            for instruction in object.instructionsIf
+                            for instruction in object.instructionsIf:
                                 recursive(instruction, newList)
-                            for instruction in object.instructionsElse
+                            for instruction in object.instructionsElse:
                                 recursive(instruction, newList)# Recursively check internal instructions
                         else:
                             if type(object) is For:
-                                for instruction in object.instructions
+                                for instruction in object.instructions:
                                     recursive(instruction, newList)# Recursively check internal instructions
                             else:
                                 if type(object) is ArrayDeclaration:
                                     if lookup(object.name,listaDeListas):
                                         print("El arreglo ya fue declarado")
                                     else:
+                                        pass
                                         # Para la construcción del AST nunca creamos objetos ArrayDeclaration, pero deberíamos y mandarle name, size y values.
                                 else:
                                     if type(object) is ArrayAssignation:
@@ -106,7 +112,7 @@ def recursive(object, listaDeListas):
                                             #Deberiamos buscar la ubicación del arreglo y verificar que el value del statement coincida con el de la variable
                                         else:
                                             print("Arreglo ",object.name, " no ha sido declarado")
-    listOfLists.insert(0,listaDeListas)
+    #listOfLists.insert(0,listaDeListas)
 
 
 def lookup(name,tableOfSymbols):
