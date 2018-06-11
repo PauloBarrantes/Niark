4384decdc028ca255ec476aace89bd8443c324d9
Paulo Barrantes <paulobarrantes98@gmail.com>
2018-06-10 20:59:23 -0600

    Cambios
    Dudas resueltas

--- semantic.py
+++ semantic.py
@@ -76,37 +76,6 @@ def semantic():
                             globalScope.insert(0,statement)
                         else:
                             print("Metodo ",statement.name," no existe")
-                    else:
-                        if type(statement) is If:
-                            # No se necesita lookup
-                            newList = listOfLists[:] #Asignamos copia de la tabla de simbolos
-                            recursive(statement, newList)# Recursively check internal instructions
-                            # No entiendo bien como va a funcionar el scope y el recursive
-                        else:
-                            if type(statement) is IfAndElse:
-                                # No se necesita lookup
-                                newList = listOfLists[:] #Asignamos copia de la tabla de simbolos
-                                recursive(statement, newList)# Recursively check internal instructions
-                                # No entiendo bien como va a funcionar el scope y el recursive
-                            else:
-                                if type(statement) is For:
-                                    # No se necesita lookup
-                                    newList = listOfLists[:] #Asignamos copia de la tabla de simbolos
-                                    recursive(statement, newList)# Recursively check internal instructions
-                                    # No entiendo bien como va a funcionar el scope y el recursive
-                                else:
-                                    if type(statement) is ArrayDeclaration:
-                                        if lookup(statement.name,listOfList):
-                                            print("El arreglo ya fue declarado")
-                                        else:
-                                            # Para la construcción del AST nunca creamos objetos ArrayDeclaration, pero deberíamos y mandarle name, size y values.
-                                    else:
-                                        if type(statement) is ArrayAssignation:
-                                            if lookup(statement.name,listOfList):
-                                                print("Es una asignacion de arreglo, vamos a asignar el valor\n")
-                                                #Deberiamos buscar la ubicación del arreglo y verificar que el value del statement coincida con el de la variable
-                                            else:
-                                                print("Arreglo ",statement.name, " no ha sido declarado")
 
 def recursive(object, listaDeListas ):
 
@@ -117,35 +86,49 @@ def recursive(object, listaDeListas ):
             else:
                 recursive(instruction,list)
     else:
-        if type(object) is
+        '''
+        Nota: El recursive lo en teoría debe hacer, es ver que es lo que estamos procesando dependiendo de eso,
+        hacemos llamados recursivos, pero de qué?
+                    - Hacemos un for que recorra toda las instrucciones (que es una lista de objetos) y le pasamos la actual lista de listas del scope.
+        '''
+        if type(object) is If:
+            # No se necesita lookup
+            # Paulo - Si se necesita, hay que recorrer todas las condiciones viendo si hay variables y hacer lookup
+            newList = listOfLists[:] #Asignamos copia de la tabla de simbolos
+            recursive(statement, newList)# Recursively check internal instructions
+            # No entiendo bien como va a funcionar el scope y el recursive
+            #no se
+        else:
+            if type(object) is IfAndElse:
+                # No se necesita lookup
+                #Paulo- sí se necesita recorrer las condiciones y ver si hay variables no declradas.
+                newList = listOfLists[:] #Asignamos copia de la tabla de simbolos
+                recursive(statement, newList)# Recursively check internal instructions
+                # No entiendo bien como va a funcionar el scope y el recursive
+                # Paulo - En el if and else se debe hacer un for para las instrucciones del if y otro para las instrucciones del Else con diferentes tabla de símbolos.
 
-    if type(object) is For:
-        pass
-
-    if type(object) is If:
-        pass
-
-    if type(object) is IfAndElse:
-        pass
-
-    if type(object) is Variable:
-        pass
-
-    if type(object) is VariableAssignation:
-        pass
-
-    if type(object) is VariableDeclaration:
-        pass
-
-    if type(object) is ArrayAssignation:
-        pass
-
-    if type(object) is ArrayDeclaration:
-        pass
-
-    if type(object) is FunctionCall:
-        pass
-
+            else:
+                if type(object) is For:
+                    # No se necesita lookup
+                    # Si se necesita para ver si se hace una condición con alguna variable, hay que verificar si esta en el tos
+                    newList = listOfLists[:] #Asignamos copia de la tabla de simbolos
+                    recursive(statement, newList)# Recursively check internal instructions
+                    # No entiendo bien como va a funcionar el scope y el recursive
+                else:
+                    if type(object) is ArrayDeclaration:
+                        if lookup(statement.name,listOfList):
+                            print("El arreglo ya fue declarado")
+                        else:
+                            # Para la construcción del AST nunca creamos objetos ArrayDeclaration, pero deberíamos y mandarle name, size y values.
+                    else:
+                        if type(object) is ArrayAssignation:
+                            if lookup(statement.name,listOfList):
+                                print("Es una asignacion de arreglo, vamos a asignar el valor\n")
+                                #Deberiamos buscar la ubicación del arreglo y verificar que el value del statement coincida con el de la variable
+                            else:
+                                print("Arreglo ",statement.name, " no ha sido declarado")
+                        else:
+                            ## Hay que agregar Variables....
 
 def lookup(name,tableOfSymbols):
     encontrado = False
