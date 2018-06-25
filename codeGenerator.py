adfaf6b6858a7824330b481d7ec80193b62f1150
Paulo Barrantes <paulobarrantes98@gmail.com>
2018-06-25 17:46:50 -0600

    BitMap and PATADA INICIAL DEL GENERDAOR DE C´ODIGO!!

--- codeGenerator.py
+++ codeGenerator.py
@@ -1,18 +1,6 @@
-BitMap[32]
-
-class bit:
-    nameReg = ''
-    index = ''
-    state = False
-    def __init__(self, nameReg, index, state):
-        self.nameReg = nameReg
-        self.index = index
-        self.state = state
+from BitMap import *
+from semantic import *
 
 def codeGenerator():
-    pass
-
-
-def fillBitMap(arg):
-    pass
+    
 
